ff3a771085b372ca547a2c3fa6032a51d6be81df
Christopher Hutchinson <cshutchinson@gmail.com>
2016-02-23 09:23:29 -0700

    mods to express server for proper start

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var Express = require("express")
 var Socket = require("socket.io")
 var http = require("http")
 var unirest = require('unirest')
-var db = require('monk')('localhost/realState')
+// var db = require('monk')('localhost/realState')
 
 var app = Express()
 var server = http.Server(app)
