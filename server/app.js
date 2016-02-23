6ca6218e620583ec4775492ce3dbaf7c9369e5b5
Christopher Hutchinson <cshutchinson@gmail.com>
2016-02-23 09:09:59 -0700

    basic express server config

---
+++ server/app.js
@@ -0,0 +1,16 @@
+var Express = require("express")
+var Socket = require("socket.io")
+var http = require("http")
+var unirest = require('unirest')
+var db = require('monk')('localhost/realState')
+
+var app = Express()
+var server = http.Server(app)
+var io = Socket(server)
+
+
+app.use(Express.static("./client"))
+
+server.listen(8080, function () {
+  console.log("listening on 8080")
+})
